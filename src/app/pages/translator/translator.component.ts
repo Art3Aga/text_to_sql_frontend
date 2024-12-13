975a3a43160b162bd5b2d76701eb167a2a2edfee
aarteaga <aarteaga@gbm.net>
2024-12-13 17:59:57 -0600

    se integra la api del modelo y se realiza funcionalidad para ejecutar la consulta sql

--- src/app/pages/translator/translator.component.ts
+++ src/app/pages/translator/translator.component.ts
@@ -5,6 +5,11 @@ import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatInputModule} from '@angular/material/input';
 import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
 import { FormsModule } from '@angular/forms';
+import {MatButtonModule} from '@angular/material/button';
+import {MatTableDataSource, MatTableModule} from '@angular/material/table';
+import { CommonModule } from '@angular/common';
+
+
 
 
 
@@ -13,7 +18,11 @@ import { FormsModule } from '@angular/forms';
 @Component({
   selector: 'app-translator',
   standalone: true,
-  imports: [ MatIconModule, MatFormFieldModule, MatInputModule, MatProgressSpinnerModule, FormsModule ],
+  imports: [
+    MatIconModule, MatFormFieldModule, MatInputModule,
+    MatProgressSpinnerModule, FormsModule, MatButtonModule,
+    MatTableModule, CommonModule
+  ],
   templateUrl: './translator.component.html',
   styleUrl: './translator.component.scss',
 })
@@ -22,12 +31,17 @@ export class TranslatorComponent implements OnInit {
   text: string = '';
   loading: boolean = false;
   queryResult: string = '';
+  data: any[] = [];
+  showButtonExecute: boolean = false;
+
+  displayedColumns: string[] = [];
+  dataSource = new MatTableDataSource<any>([]);
 
   constructor(private readonly api: ApiService) {}
 
 
   ngOnInit(): void {
-
+    //this.executeQuery()
   }
 
   translateSql() {
@@ -36,14 +50,36 @@ export class TranslatorComponent implements OnInit {
     this.api.translateToSql(this.text).subscribe({
       next: (data) => {
         this.loading = false;
+        this.showButtonExecute = true;
         this.queryResult = data.result;
       },
       error: (err) => {
+        this.showButtonExecute = false;
         this.loading = false;
         this.queryResult = `Error en el modelo: ${err}`;
         console.log(`Error en la API: ${err}`);
       },
     })
+  }
+
+  executeQuery(query: string) {
+    this.api.executeQuery(query).subscribe({
+      next: (data) => {
+        console.log(data)
+        this.displayedColumns = data.columns;
+        this.dataSource = data.rows;
+      },
+      error: (err) => {
+        console.log(`Error en la API: ${err}`);
+      },
+    })
+  }
+
+
+  clearData() {
+    this.showButtonExecute = false;
+    this.text = '';
+    this.queryResult = '';
   }
 
 }
--- src/app/services/api/api.service.ts
+++ src/app/services/api/api.service.ts
@@ -6,13 +6,19 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 })
 export class ApiService {
 
-  private apiUrl = 'http://127.0.0.1:5000/tuning';
+  private readonly apiUrl = 'http://127.0.0.1:5000';
 
   constructor(private http: HttpClient) { }
 
   translateToSql(text: string) {
     const headers = new HttpHeaders({ 'Content-Type': 'application/json' });
-    return this.http.post<any>(this.apiUrl, { query: text }, { headers });
+    return this.http.post<any>(`${this.apiUrl}/tuning`, { query: text }, { headers });
+  }
+
+
+  executeQuery(query: string) {
+    const headers = new HttpHeaders({ 'Content-Type': 'application/json' });
+    return this.http.post<any>(`${this.apiUrl}/execute`, { query: query }, { headers });
   }
 
 }
